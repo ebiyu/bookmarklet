ff82210a40d18aec14a73f46b6ae78425468d96d
Ebihara Yusuke <me@ebiyuu.com>
2022-09-05 08:30:32 +0900

    google-translate

---
+++ src/scripts/google-translate.js
@@ -0,0 +1,1 @@
+window.open(`https://translate.google.com/translate?sl=auto&tl=ja&hl=en&u=${encodeURIComponent(location.href)}`)
---
+++ src/scripts/google-translate.md
@@ -0,0 +1,8 @@
+---
+title: サイトをGoogle翻訳
+layout: script.njk
+actionName: サイトをGoogle翻訳
+---
+
+現在表示しているサイトをGoogle翻訳します。
+
